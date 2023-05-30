59bcb1bed3df5cfa429b8ab98357acab172cec0d
coder <usmccoder@gmail.com>
2023-05-29 20:21:17 -0700

    deploy.sh update

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 REGISTRY="sgtcoder"
 PROJECT_NAME="wordpress-apache"
 
-docker build --no-cache -t $REGISTRY/$PROJECT_NAME . | exit 1
+docker build --no-cache -t $REGISTRY/$PROJECT_NAME . || exit 1
 docker push $REGISTRY/$PROJECT_NAME
